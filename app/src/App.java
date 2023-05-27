8bc29be583f0f4de1a0280a61b9af36f4af7b4b5
thenPie <118397975+thenPie@users.noreply.github.com>
2023-05-27 14:07:06 +0300

    beautify

--- app/src/App.java
+++ app/src/App.java
@@ -1,55 +1,76 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
+import Domain.ComplexNumber;
 import Domain.CNaddition;
+import Domain.CNmultiplication;
 import Domain.CNdivision;
 import Domain.CNfactory;
-import Domain.CNmultiplication;
-import Domain.ComplexNumber;
 import View.View;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
 
         ComplexNumber cn1 = new ComplexNumber(0, 0);
         ComplexNumber cn2 = new ComplexNumber(0, 0);
         ComplexNumber res = new ComplexNumber(0, 0);
+        
         Scanner input = new Scanner(System.in);
         int choice;
         double r, i;
+        
         boolean working = true;
         while (working) {
             View.start();
+            
             View.inputCN1();
             View.inputReal();
             r = input.nextDouble();
+
             View.inputImaginery();
             i = input.nextDouble();
+            
             cn1 = CNfactory.createCN(r, i);
+            
             View.inputCN2();
             View.inputReal();
             r = input.nextDouble();
+            
             View.inputImaginery();
             i = input.nextDouble();
+            
             cn2 = CNfactory.createCN(r, i);
-            View.actions();
-            choice = input.nextInt();
-            if (choice == 1) {
-                res = CNaddition.add(cn1, cn2);
-            } else if (choice == 2) {
-                res = CNmultiplication.multiplication(cn1, cn2);
-            } else if (choice == 3) {
-                res = CNdivision.division(cn1, cn2);
+            
+            boolean actions = true;
+            while (actions) {
+                View.actions();
+                
+                choice = input.nextInt();
+                if (choice == 1) {
+                    res = CNaddition.add(cn1, cn2);
+                    actions = false;
+                } else if (choice == 2) {
+                    res = CNmultiplication.multiplication(cn1, cn2);
+                    actions = false;
+                } else if (choice == 3) {
+                    res = CNdivision.division(cn1, cn2);
+                    actions = false;
+                } else {
+                    System.out.println("Чего?");
+                }
+
             }
+
             cn1.showCN();
             cn2.showCN();
             res.showCN();
+
             View.againOr();
             choice = input.nextInt();
             if (choice == 1) {
                 // continue
             } else {
+                input.close();
                 working = false;
             }
         }
--- app/src/Domain/Actions.java
+++ app/src/Domain/Actions.java
@@ -29,7 +29,7 @@ public class Actions {
         return cn;
     }
 
-    interface iAction {
+    public interface iAction {
         void act();
     }
 
